2d0fefa4dceb94325b890b2d0e3c96795cc94375
Ruoda Yuan <ruoda.yuan@gmail.com>
2022-02-04 23:56:10 -0600

    Fixed drop tables

--- p3/drop_tables.sql
+++ p3/drop_tables.sql
@@ -1,13 +1,1 @@
-DROP TABLE products;
-DROP TABLE images;
-DROP TABLE variants;
-DROP TABLE collections;
-DROP TABLE orders;
-DROP TABLE customers;
-DROP TABLE sellers;
-DROP TABLE coll_prod;
-DROP TABLE sell_prod;
-DROP TABLE ord_prod;
-DROP TABLE ord_cust;
-
 DROP TABLE IF EXISTS products, images, variants, collections, orders, customers, sellers, coll_prod, sell_prod, ord_prod, ord_cust;
