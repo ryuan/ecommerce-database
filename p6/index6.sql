6c79265ba2f9fe95a4d3a064e81ca0c2ca6b554f
Ruoda Yuan <ruoda.yuan@gmail.com>
2022-02-25 23:54:39 -0600

    Index 2 created as well

--- p6/index6.sql
+++ p6/index6.sql
@@ -1,4 +1,9 @@
 --This index lets us quickly lookup pricing and quantity data.
 --Orders are currently mapped to variants, and variants are mapped to products with foreign key.
 --The index lets us quickly relate the two for both the first query and third query.
-CREATE UNIQUE INDEX idx_variants ON variants (v_id, p_id, quantity, price);
+CREATE INDEX idx_variants ON variants (v_id, p_id, quantity, price);
+
+--The largest relation by far is our coll_prod relation, which maps collections with all its products.
+--As such, the second query takes a long time to run due to the massive 800,000 tuple dataset.
+--To speed up the query, index the collection name so that references to it is immediate.
+CREATE INDEX idx_collections ON collections (c_name);
