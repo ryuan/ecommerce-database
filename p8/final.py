2dac171840d268e3cb7592cbee38e72169358546
Ruoda Yuan <ruoda.yuan@gmail.com>
2022-03-10 03:32:25 -0600

    Delete works

--- p8/final.py
+++ p8/final.py
@@ -8,7 +8,24 @@ con = sqlite3.connect('ecommerce.db')
 cur = con.cursor()
 
 PER_PAGE = 20
-TOTAL, = cur.execute('SELECT COUNT(*) FROM products').fetchone()
+
+
+@app.route('/delete')
+def delete():
+    p_id = request.query.get('p_id')
+
+    # fetch just the name of the p_id product to display confirmation in view
+    product = cur.execute('SELECT p_name FROM products WHERE p_id =' + p_id + ';')
+    p_name = product.fetchone()[0]
+
+    cur.execute(f'DELETE FROM products WHERE p_id = { p_id };')
+    con.commit()
+
+    parameters = {
+        'p_name' : p_name,
+        }
+
+    return template("delete.html", parameters)
 
 
 @app.post('/search')
@@ -45,6 +62,10 @@ def search():
 
 @app.route('/product/<p_id>')
 def detail(p_id):
+    # fetch just the name of the p_id product for meta tag title
+    product = cur.execute('SELECT p_name FROM products WHERE p_id =' + p_id + ';')
+    p_name = product.fetchone()[0]
+
     # fetch the db data for p_id
     product = cur.execute('SELECT * FROM products WHERE p_id =' + p_id + ';')
     keys = ('p_id', 'p_name', 'type', 'vendor', 'p_description')
@@ -52,10 +73,12 @@ def detail(p_id):
 
     parameters = {
         'p_id' : p_id,
+        'p_name' : p_name,
         'product' : product
         }
 
     return template("product.html", **parameters)
+
 
 @app.route('/')
 @app.route('/<page:int>')
@@ -63,6 +86,7 @@ def main(page=0):
     # number of records per page
     per = int(request.query.per or PER_PAGE)
     start, end = page*per, (page+1)*per
+    total, = cur.execute('SELECT COUNT(*) FROM products').fetchone()
 
     # fetch the db data
     products = cur.execute(f'SELECT p_id, p_name, type, vendor FROM products WHERE p_id >= {start} AND p_id < {end};')
@@ -72,13 +96,15 @@ def main(page=0):
     parameters = {
         'page' : page,
         'products' : products,
-        'has_next' : end < TOTAL,
+        'has_next' : end < total,
+        'total_products' : total,
         'query_string' : '?' + request.query_string,
         }
 
     return template('index.html', **parameters)
 
-# Static CSS files
+
+# Static CSS and favicon files
 @app.route('/static/css/<filename:re:.*\.css>')
 def send_css(filename):
     return static_file(filename, root='static/css')
@@ -86,6 +112,7 @@ def send_css(filename):
 @app.route('/static/images/<filename:re:.*\.ico>')
 def send_favicon(filename):
     return static_file(filename, root='static/images')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug = True)
