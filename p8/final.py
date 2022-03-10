b0b2d3175c5bd3219bfb046bc655ac2de6b17e27
Ruoda Yuan <ruoda.yuan@gmail.com>
2022-03-09 19:01:11 -0600

    Search implemented

--- p8/final.py
+++ p8/final.py
@@ -1,5 +1,5 @@
 import bottle
-from bottle import request
+from bottle import request, jinja2_template as template
 import sqlite3
 
 app = bottle.Bottle()
@@ -7,52 +7,66 @@ app = bottle.Bottle()
 con = sqlite3.connect('ecommerce.db')
 cur = con.cursor()
 
-PER_PAGE = 20
-USER_TOTAL, = cur.execute('SELECT COUNT(*) FROM customers').fetchone()
+PER_PAGE = 30
+TOTAL, = cur.execute('SELECT COUNT(*) FROM products').fetchone()
 
-@app.route('/customer/<user_id>')
-def detail(user_id):
-    customer = cur.execute('SELECT cust_id, cust_name, cust_email, def_bill_add, def_ship_add, cust_phone FROM customers WHERE cust_id=' + user_id)
 
-    html = "<a href=\"/\">Back</a>"
-    for row in customer:
-        html += "<h1>" + row[1] + "</h1> <br/> <table>"
-        for cell in row[2:]:
-            html += "<tr> <td>" + str(cell) + "</td> </tr> "
-        html += "</table>"
+@app.post('/search')
+def search():
+    # collect GET query data
+    if request.forms.get('p_name') != None:
+        p_name = request.forms.get('p_name')
+    if request.forms.get('type') != None:
+        type = request.forms.get('type')
+    if request.forms.get('vendor') != None:
+        vendor = request.forms.get('vendor')
 
-    return html
+    results = cur.execute('SELECT p_id, p_name, type, vendor FROM products WHERE p_name LIKE \"%' + p_name.strip() + '%\" AND type LIKE \"%' + type.strip() + '%\" AND vendor LIKE \"%' + vendor.strip() + '%\" LIMIT 20;')
+
+    keys = ('p_id', 'p_name', 'type', 'vendor')
+    results = (dict(zip(keys, result)) for result in results)
+
+    parameters = {
+        'results' : results,
+        }
+
+    return template("search.html", **parameters)
+
+
+@app.route('/product/<p_id>')
+def detail(p_id):
+    # fetch the db data for p_id
+    product = cur.execute('SELECT * FROM products WHERE p_id =' + p_id + ';')
+    keys = ('p_id', 'p_name', 'type', 'vendor', 'p_description')
+    product = (dict(zip(keys, data)) for data in product)
+
+    parameters = {
+        'p_id' : p_id,
+        'product' : product
+        }
+
+    return template("product.html", **parameters)
 
 @app.route('/')
 @app.route('/<page:int>')
 def main(page=0):
     # number of records per page
     per = int(request.query.per or PER_PAGE)
-    start, end = page*PER_PAGE, (page+1)*PER_PAGE
+    start, end = page*per, (page+1)*per
 
-    contacts = cur.execute(f'SELECT cust_id, cust_name, cust_email, cust_phone FROM customers WHERE cust_id >= {start} AND cust_id < {end}')
-    has_next = end < USER_TOTAL
-    query_string = '?per=' + str(per)
+    # fetch the db data
+    products = cur.execute(f'SELECT p_id, p_name, type, vendor FROM products WHERE p_id >= {start} AND p_id < {end};')
+    keys = ('p_id', 'p_name', 'type', 'vendor')
+    products = (dict(zip(keys, product)) for product in products)
 
-    html = "<h1>Basic Customer Contacts</h1> <br/>"
-    html += "<h2>" + str(page) + "<h2> <br/>"
-    html += "<table>"
-    for row in contacts:
-        html += "<tr>"
-        for cell in row[1:]:
-            html += "<td>" + str(cell) + "</td>"
-        html += "<td> <a href=\"/customer/" + str(row[0]) + "\">View Details</a> </td> </tr>"
-    html += "</table>"
+    parameters = {
+        'page' : page,
+        'products' : products,
+        'has_next' : end < TOTAL,
+        'query_string' : '?' + request.query_string,
+        }
 
-    if page > 0:
-        if page == 1:
-            html += "<a href=\"/" + query_string + "\">prev</a>"
-        else:
-            html += "<a href=\"" + str((page-1)) + query_string + "\">prev</a>"
-    if has_next:
-        html += "<a href=\"" + str((page+1)) + query_string + "\">next</a>"
+    return template('home.html', **parameters)
 
-    return html
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug = True)
